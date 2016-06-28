c555aa506466d4712a512101b89e1abdb3c0380b
cedricbrx <cedricbr@hotmail.com>
2016-06-28 17:47:34 +0200

    Update network_vendor.rb

--- manifests/files/usr/lib/ruby/vendor/facter/network_vendor.rb
+++ manifests/files/usr/lib/ruby/vendor/facter/network_vendor.rb
@@ -1,18 +1,8 @@
 # Fact: network
-#
-# Purpose:
-# Get IP, network and netmask information for available network
-# interfacs.
-#
-# Resolution:
-#  Uses 'facter/util/ip' to enumerate interfaces and return their information.
-#
-# Caveats:
-#
 
-Facter.add(:network_gateway) do
+Facter.add(:network_vendor) do
   confine :kernel => [ :linux, :"gnu/kfreebsd" ]
   setcode do
-    Facter::Util::Resolution.exec('arp -a `route -n | tail -n +3 | tr -s " " | cut -f2 -d " " | grep -v 0.0.0.0` |  tr -s " " | cut -f4 -d " "')
+    Facter::Util::Resolution.exec('sudo lshw | grep -i -n2 "display" | grep -E '[A-Z]{2}[0-9]{3}' | tr -s " " | tr '[:upper:]' '[:lower:]' | cut -f3 -d " " | uniq')
   end
 end
