172e2453f060a98360217b5a8b78cb44e20aea25
cedricbrx <cedricbr@hotmail.com>
2016-06-28 17:50:49 +0200

    Delete network_vendor.rb

--- manifests/files/usr/lib/ruby/vendor/facter/network_vendor.rb
+++
@@ -1,8 +0,0 @@
-# Fact: network
-
-Facter.add(:network_vendor) do
-  confine :kernel => [ :linux, :"gnu/kfreebsd" ]
-  setcode do
-    Facter::Util::Resolution.exec('sudo lshw | grep -i -n2 "display" | grep -E '[A-Z]{2}[0-9]{3}' | tr -s " " | tr '[:upper:]' '[:lower:]' | cut -f3 -d " " | uniq')
-  end
-end
