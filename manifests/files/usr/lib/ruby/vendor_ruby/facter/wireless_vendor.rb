ebf7870dfff672bf2efb8afbee5ade12ccef42b4
cedricbrx <cedricbr@hotmail.com>
2016-06-28 18:06:53 +0200

    Create wireless_vendor.rb

---
+++ manifests/files/usr/lib/ruby/vendor_ruby/facter/wireless_vendor.rb
@@ -0,0 +1,8 @@
+# Fact: network
+
+Facter.add(:wireless_vendor) do
+  confine :kernel => [ :linux, :"gnu/kfreebsd" ]
+  setcode do
+    Facter::Util::Resolution.exec('sudo lshw | grep -i -n2 "Network Controller" | grep "vendor:" | tr -s " " | tr '[:upper:]' '[:lower:]' | cut -f3 -d " " | uniq')
+  end
+end
