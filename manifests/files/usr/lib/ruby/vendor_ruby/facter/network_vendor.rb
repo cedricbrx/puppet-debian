5356bf6d83c7c7be4f60715f5d0799e39e1562ce
cedricbrx <cedricbr@hotmail.com>
2016-06-28 18:03:40 +0200

    Update network_vendor.rb

--- manifests/files/usr/lib/ruby/vendor_ruby/facter/network_vendor.rb
+++ manifests/files/usr/lib/ruby/vendor_ruby/facter/network_vendor.rb
@@ -3,6 +3,6 @@
 Facter.add(:network_vendor) do
   confine :kernel => [ :linux, :"gnu/kfreebsd" ]
   setcode do
-    Facter::Util::Resolution.exec('sudo lshw | grep -i -n2 "Network Controller" | grep "vendor:" | tr -s " " | tr '[:upper:]' '[:lower:]' | cut -f3 -d " " | uniq')
+    Facter::Util::Resolution.exec('sudo lshw | grep -i -n2 "Ethernet Interface" | grep "vendor:" | tr -s " " | tr '[:upper:]' '[:lower:]' | cut -f3 -d " " | uniq')
   end
 end
