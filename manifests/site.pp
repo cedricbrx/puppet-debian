37172fc694cc89bac17b8c24812cd521e5fa6f88
cedricbrx <cedricbr@hotmail.com>
2016-08-03 22:45:00 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -256,21 +256,21 @@ class install {
 	}
 	if $network_vendor == 'realtek' {
 		if $wireless_vendor == 'intel' {
-			$firmware_packages = ['firmware-iwlwifi','firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
+			firmware_packages = ['firmware-iwlwifi','firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
 		}
 		else {
-			$firmware_packages = ['firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
+			firmware_packages = ['firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
 		}
 	}	
 	elsif $network_vendor == 'intel' {
 		if $wireless_vendor == 'realtek' {
-			$firmware_packages = ['firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
+			firmware_packages = ['firmware-realtek','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
 		}
 		elsif $wireless_vendor == 'intel' {
-			$firmware_packages = ['firmware-iwlwifi','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
+			firmware_packages = ['firmware-iwlwifi','firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
 		}
 		else {
-			$firmware_packages = ['firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
+			firmware_packages = ['firmware-linux-free','firmware-misc-nonfree','firmware-linux-nonfree']
 		}
 	}
 	package {$firmware_packages:
