40dd379fc10a232f8564d08764aa21aef38dbe15
cedricbrx <cedricbr@hotmail.com>
2016-08-04 13:21:39 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,17 @@
 node default {
   include facter
+  include apt
 }
 
 class facter{
+  file {"/usr/lib/ruby/vendor_ruby/facter/":
+    owner => root,
+    mode => '644',
+    recurse => true,
+}
 
+class apt {
+  Package {"apt-transport-https":
+    ensure => installed,
+  }
 }
