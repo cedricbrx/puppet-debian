465adc2fb6201206ce748762d905acbcf44ec276
cedricbrx <cedricbr@hotmail.com>
2017-03-17 10:03:29 +0100

    Delete wireless_vendor.rb

--- manifests/files/usr/lib/ruby/vendor_ruby/facter/wireless_vendor.rb
+++
@@ -1,8 +0,0 @@
-# Fact: wireless_vendor
-
-Facter.add(:wireless_vendor) do
-  confine :kernel => [ :linux, :"gnu/kfreebsd" ]
-  setcode do
-    Facter::Util::Resolution.exec('lspci | grep -i "Network Controller" | tr -s " " | tr "[:upper:]" "[:lower:]" | cut -f4 -d " " | uniq')
-  end
-end
