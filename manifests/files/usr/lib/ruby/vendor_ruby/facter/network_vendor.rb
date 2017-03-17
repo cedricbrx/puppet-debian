1ce2325a9be222d22bf86d3b4554732e737c4827
cedricbrx <cedricbr@hotmail.com>
2017-03-17 10:03:23 +0100

    Delete network_vendor.rb

--- manifests/files/usr/lib/ruby/vendor_ruby/facter/network_vendor.rb
+++
@@ -1,8 +0,0 @@
-# Fact: network_vendor
-
-Facter.add(:network_vendor) do
-  confine :kernel => [ :linux, :"gnu/kfreebsd" ]
-  setcode do
-    Facter::Util::Resolution.exec('lspci | grep -i "Ethernet Controller" | tr -s " " | tr "[:upper:]" "[:lower:]" | cut -f4 -d " " | uniq')
-  end
-end
