0be709b88710815ca273cf22d786ed4a610d16c7
cedricbrx <cedricbr@hotmail.com>
2017-03-17 10:03:11 +0100

    Delete graphic_chipset.rb

--- manifests/files/usr/lib/ruby/vendor_ruby/facter/graphic_chipset.rb
+++
@@ -1,8 +0,0 @@
-# Fact: graphic_chipset
-
-Facter.add(:graphic_chipset) do
-  confine :kernel => [ :linux, :"gnu/kfreebsd" ]
-  setcode do
-    Facter::Util::Resolution.exec('lspci | grep -i "VGA compatible controller" | tr -s " " | tr "[:upper:]" "[:lower:]" | grep -v "aspeed" | cut -f7 -d " " | uniq')
-  end
-end
