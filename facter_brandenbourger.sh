46e153996094d47cc95b7dc30e6be8143457be1b
cedricbrx <cedricbr@hotmail.com>
2017-03-30 14:46:33 +0200

    Update facter_brandenbourger.sh

--- facter_brandenbourger.sh
+++ facter_brandenbourger.sh
@@ -2,7 +2,8 @@
 
 MAC_ADDRESS=`cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address`
 #NET-TOOLS needed, please change to a different command from arp
-GATEWAY_MAC_ADDRESS=`arp -n $(ip route show default | awk '/default/ {print $3}') | tail -n 1 | awk '{print $3}'`
+#GATEWAY_MAC_ADDRESS=`arp -n $(ip route show default | awk '/default/ {print $3}') | tail -n 1 | awk '{print $3}'`
+GATEWAY_MAC_ADDRESS=`ip n show | grep $(ip route show default | awk '/default/ {print $3}') | awk '{print $5}'`
 SYNOLOGY_CLOUD_STATION_ONLINE_VERSION=`curl -s http://dedl.synology.com/download/Tools/CloudStationDrive/ | perl -wlne 'print "$1" if /(\d\.\d.\d-\d\d\d\d)/' | tail -n1`
 SYNOLOGY_ASSISTANT_ONLINE_VERSION=`curl -s http://dedl.synology.com/download/Tools/Assistant/ | perl -wlne 'print "$1" if /(\d.\d-\d\d\d\d+)/' | tail -n1`
 SYNOLOGY_CLOUD_STATION_VERSION=`dpkg -l synology-cloud-station | awk '{print $3}' | tail -n 1`
